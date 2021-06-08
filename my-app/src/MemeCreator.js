57bff7e48aec242247c7ec6683435b106a545477
Catherine Beimfohr <cjbeimfohr13@gmail.com>
2021-06-08 10:31:11 -0400

    adding header and memecreator js and functions

--- my-app/src/MemeCreator.js
+++ my-app/src/MemeCreator.js
@@ -1,5 +1,21 @@
-import React from "react"
+import React, {Component} from "react"
 
+class MemeCreator extends Component {
+    constructor(){
+      super()
+      this.state={
+       
+      }
+      
+    }
+    componentDidMount(){
+     
+    }
+    handleChange(){
+     
+    }
+    render(){
 
+    }
 
 export default MemeCreator
