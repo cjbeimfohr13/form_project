f10eb1d4a1b75bc7dd0e8af666b96186ece28117
Catherine Beimfohr <cjbeimfohr13@gmail.com>
2021-06-08 14:13:05 -0400

    adding form and handleChange plus  binding

--- my-app/src/MemeCreator.js
+++ my-app/src/MemeCreator.js
@@ -6,23 +6,42 @@ class MemeCreator extends Component {
       this.state={
         topText:"",
         bottomText:"",
-        randomImg:"http://i.imgflip.com/1bij.jpg"
+        randomImg:"http://i.imgflip.com/1bij.jpg",
+        allMemeImgs:[]
       }
+      this.handleChange=this.handleChange.bind(this)
       
     }
     componentDidMount(){
         fetch("https://api.imgflip.com/get_memes")
             .then(response=>response.json())
-            .then()
+            .then(response=>{
+                const {memes}= response.data
+                this.setState({
+                    allMemeImgs:memes
+                })
+            })
         
      
     }
-    handleChange(){
-     
+    handleChange(event){
+        const {name, value}= event.target
+        this.setState({
+            [name]:value
+        })
+    }
+    handleSubmit(){
+
     }
     render(){
         return(
-            <h1>Hey!</h1>
+            <div>
+            <form>
+                <input name="topText" placeholder="Top Text" value={this.state.topText} type="text" onChange={this.handleChange()}/>
+                <input name="bottomText" placeholder="Bottom Text" value={this.state.bottomText} type="text" onChange={this.handleChange()}/>
+                <button>SUBMIT</button>
+            </form>
+           </div>
         )
     }
 }
