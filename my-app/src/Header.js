ebb32b081f7765a7377db017075e5b1f187326c0
Catherine Beimfohr <cjbeimfohr13@gmail.com>
2021-06-08 12:52:18 -0400

    updating state and adding api link

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -3,7 +3,10 @@ import React from "react"
 function Header(){
     return(
         <div>
-            Header
+            <header>
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.timesofindia.com%2Flife-style%2Fcant-have-enough-of-laughter-emojis-say-mumbaikars%2Farticleshow%2F75525221.cms&psig=AOvVaw0ZdXKnkVEZrqeqql_xxotM&ust=1623250173485000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOCb8aOkiPECFQAAAAAdAAAAABAK" alt="Laughing"/>
+                <p>Show Your Creativity Below!</p>
+            </header>
         </div>
     )
 }
--- my-app/src/MemeCreator.js
+++ my-app/src/MemeCreator.js
@@ -4,18 +4,26 @@ class MemeCreator extends Component {
     constructor(){
       super()
       this.state={
-       
+        topText:"",
+        bottomText:"",
+        randomImg:"http://i.imgflip.com/1bij.jpg"
       }
       
     }
     componentDidMount(){
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response=>response.json())
+            .then()
+        
      
     }
     handleChange(){
      
     }
     render(){
-
+        return(
+            <h1>Hey!</h1>
+        )
     }
-
+}
 export default MemeCreator
